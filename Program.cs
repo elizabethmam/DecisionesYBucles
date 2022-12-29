c0b86ae0dad185f0f8938eac4852f9c29deb0503
elizabethmam <elizabethguerrero.mam@gmail.com>
2022-12-29 10:19:01 -0600

    fin de bucles y decisiones

--- Program.cs
+++ Program.cs
@@ -82,7 +82,199 @@ switch(lugar)
         break;
 }
 
-//Operadores relacionales
+//Expresiones switch
+string mensajeLugar = lugar switch
+{
+    1 => "Primer lugar",
+    2 => "Eres segundo lugar",
+    3 => "Tercer lugar",
+    4 or 5 => "Casi lo logras",
+    _ => "Fin" //Siempre debe haber un menssaje por defecto o lanzará un error de excepción.
+};
+Console.WriteLine(mensajeLugar);
+
+//Console.WriteLine(lugar switch //También se puede hacer de esta manera.
+//{
+//    1 => "Primer lugar",
+//    2 => "Eres segundo lugar",
+//    3 => "Tercer lugar",
+//    4 or 5 => "Casi lo logras",
+//    _ => "Fin" //Siempre debe haber un menssaje por defecto o lanzará un error de excepción.
+//});
+
+//Patrones relacionales: para comparar el resultado de una expresión con una constante especificada.
 int temperatura = 35;
 
-string mensaje 
+string mensaje = temperatura switch
+{
+    37 => "No tienes ninguna condición",
+    > 37 => "Tienes fiebre",
+    < 37 => "Puede que tengas baja el azúcar"
+};
+Console.WriteLine(mensaje);
+
+//Patrones lógicos: para probar si una expresión coincide con una combinación lógica de patrones.
+string? palabra = "Cualquier palabra.";
+
+if (palabra is not null)
+{
+    Console.WriteLine(palabra.ToUpper());
+}
+
+Console.WriteLine("El programa ha terminado.");
+
+int temp2 = 30;
+
+string message = temp2 switch //se debe cubrir todos los casos o lanza una advertencia
+{
+    37 => "No tienes ninguna condición",
+    > 37 and < 39 => "Tienes fiebre",
+    >= 39 and < 43 => "Debes ir a lx médicx.",
+    >= 43 => "rarx",
+    < 37 => "Puede que tengas baja el azúcar"
+};
+Console.WriteLine(message);
+
+DateTime hoy = DateTime.Today;
+
+string estacion = hoy.Month switch
+{
+    12 or 1 or 2 => "Invierno",
+    3 or 4 or 5 => "Primavera",
+    6 or 7 or 8 => "Verano",
+    9 or 10 or 11 => "Otoño",
+    _ => throw new ApplicationException("Mes incorrecto.") // "_" es la variable de descarte.
+};
+Console.WriteLine(estacion);
+
+
+//Bucle while
+int contador = 1; //Inicialización de la variable
+
+while (contador <= 10) //Condición
+{ //Bloque de código
+    Console.WriteLine(contador);
+    //Modificación de la variable inicializada.
+    contador++; //ir cambiando la variable para que en un momento determinado se evalúe como falso y salga del bucle; evitar un bucle infinito.
+
+
+}
+
+decimal balance = 200m;
+decimal interes = 1.07m;
+int contadorInteres = 1;
+
+while(contadorInteres <= 10)
+{
+    balance *= interes;
+    contadorInteres++;
+}
+Console.WriteLine(balance);
+
+//Bucle do while: el código se ejecuta al menos una vez, incluso si la condición es falsa.
+do
+{
+    Console.WriteLine(contador);
+    contador++;
+
+} while (contador <= 10);
+
+//do
+//{
+//    Console.WriteLine(contador);
+//    contador++;
+
+//} while (contador > 10);
+
+
+//Bucle for
+
+//for (inicialización; condición; modificación de la variable inicializada) { bloque de código que se repetirá }
+for (int i = 0; i <= 10; i++)
+{
+    //El scope (alcance) de la variable "i" sólo funciona dentro del bloque de código "for".
+    Console.WriteLine(i);
+}
+
+//Bucle forEach: útil para colecciones de valores. Permite iterar los elementos de una colección de datos.
+//String: cadena de caracteres, por lo tanto se puede iterar caracter por caracter con un forEach.
+//Colección: varios elementos que podemos recorrer.
+
+string miNombre = "Elizabeth";
+ // forEach significa que intere por cada letra en la variable "miNombre"
+foreach(char letra in miNombre)
+{
+    Console.WriteLine(letra);
+}
+
+//Sentencias de salto: funcionan para condicionales y bucles.
+for (int j = 0; j <= 10; j++)
+{
+    if (j == 5)
+    {
+        break; //rompe el ciclo y muestra en pantalla hasta donde se iteró
+    }
+
+    Console.WriteLine(j);
+}
+
+for (int j = 0; j <= 10; j++)
+{
+    if (j == 5)
+    {
+        continue; //se salta el 5 y sigue con la iteración hasta que termine.
+    }
+
+    Console.WriteLine(j);
+}
+
+
+//Bucles infinitos
+
+/*while (true)
+{
+    Console.WriteLine("éste es un bucle infinito.");
+}*/
+
+/*for (; ; )
+{
+    Console.WriteLine("También es un bucle infinito.");
+}*/
+
+while (true)
+{
+    Console.WriteLine("Te damos la bienvenida a la calculadora de suma con C#.");
+    Console.Write("Escriba el primer número: ");
+    string entradaUsuario = Console.ReadLine(); //se lee lo que el usuario escribe en la consola.
+
+    if (entradaUsuario is null)
+    {
+        Console.WriteLine("Error, vuelve a comenzar.");
+        continue;
+    }
+
+    decimal numero1 = decimal.Parse(entradaUsuario);
+
+    Console.Write("Escriba el segundo número: ");
+    entradaUsuario = Console.ReadLine();
+
+    if (entradaUsuario is null)
+    {
+        Console.WriteLine("Error, vuelve a comenzar.");
+        continue;
+    }
+
+    decimal numero2 = decimal.Parse(entradaUsuario);
+
+    Console.WriteLine($"El resultado de sumar {numero1} + {numero2} es {numero1 + numero2}");
+    Console.WriteLine("¿Desea sumar otra vez? (s/n): ");
+
+    entradaUsuario = Console.ReadLine();
+
+    if (entradaUsuario == "n")
+    {
+        break;
+    }
+}
+
+Console.WriteLine("Adiós.");
